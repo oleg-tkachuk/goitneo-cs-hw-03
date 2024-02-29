e97bf05d80871c72e9211a2f4a74e55035a27903
Oleg Tkachuk <oleg-tkachuk@users.noreply.github.com>
2024-03-01 00:54:33 +0200

    chore(init.sql): formatting with pg_format

--- task-1/init.sql
+++ task-1/init.sql
@@ -1,32 +1,42 @@
 -- Creating the 'users' table
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    fullname VARCHAR(100),
-    email VARCHAR(100) UNIQUE
+    id serial PRIMARY KEY,
+    fullname varchar(100),
+    email varchar(100) UNIQUE
 );
 
 -- Creating the 'status' table
 CREATE TABLE status (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE CHECK (name IN ('new', 'in progress',
-                                            'completed', 'on hold',
-                                            'canceled', 'blocked',
-                                            'backlog', 'in review'))
+    id serial PRIMARY KEY,
+    name varchar(50) UNIQUE CHECK (name IN ('new',
+                                            'in progress',
+                                            'completed',
+                                            'on hold',
+                                            'canceled',
+                                            'blocked',
+                                            'backlog',
+                                            'in review'))
 );
 
 -- Creating the 'task' table
 CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(100),
-    description TEXT,
-    status_id INTEGER,
-    user_id INTEGER,
-    FOREIGN KEY (status_id) REFERENCES status(id),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    id serial PRIMARY KEY,
+    title varchar(100),
+    description text,
+    status_id integer,
+    user_id integer,
+    FOREIGN KEY (status_id) REFERENCES status (id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 -- Adding unique statuses to the 'status' table
-INSERT INTO status (name) VALUES ('new'), ('in progress'),
-                                 ('completed'), ('on hold'),
-                                 ('canceled'), ('blocked'),
-                                 ('backlog'), ('in review');
+INSERT INTO status (name)
+    VALUES ('new'),
+    ('in progress'),
+    ('completed'),
+    ('on hold'),
+    ('canceled'),
+    ('blocked'),
+    ('backlog'),
+    ('in review');
+
