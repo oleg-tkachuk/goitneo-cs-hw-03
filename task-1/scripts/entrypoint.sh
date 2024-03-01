9de5e0f49fd544d68f62e4ff7acca6eb1733ee8a
Oleg Tkachuk <oleg-tkachuk@users.noreply.github.com>
2024-03-01 18:10:28 +0200

    fix(entrypoint.sh): input variables; refactor

--- task-1/scripts/entrypoint.sh
+++ task-1/scripts/entrypoint.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-POSTGRES_HOST_AND_PORT=${POSTGRES_HOST_AND_PORT:-"postgres:5432"}
-MAX_ATTEMPTS=30  # For example, a maximum of 30 attempts (3 seconds)
+POSTGRES_HOST=${POSTGRES_HOST:-"postgres"}
+POSTGRES_PORT=${POSTGRES_PORT:-"5432"}
+
+MAX_ATTEMPTS=300  # For example, a maximum of 30 attempts (3 seconds)
 attempt_num=1
 
 SEED_SCRIPT_PATH="/app/seed.py"
 
 echo "Waiting for postgres to start..."
-echo "Connections details: $POSTGRES_HOST_AND_PORT"
+echo "Connections details: ${POSTGRES_HOST_AND_PORT}"
 
-while ! nc -z $POSTGRES_HOST_AND_PORT; do
+while ! nc -z ${POSTGRES_HOST} ${POSTGRES_PORT}; do
   sleep 0.1
-  if [ $attempt_num -eq $MAX_ATTEMPTS ]; then
-    echo "Timeout waiting for PostgreSQL at $POSTGRES_HOST_AND_PORT"
+  if [[ ${attempt_num} -eq ${MAX_ATTEMPTS} ]]; then
+    echo "Timeout waiting for PostgreSQL at ${POSTGRES_HOST_AND_PORT}"
     exit 1
   fi
-  echo "PostgreSQL not available yet, retrying ($attempt_num/$MAX_ATTEMPTS)..."
+  echo "PostgreSQL not available yet, retrying (${attempt_num}/${MAX_ATTEMPTS})..."
   ((attempt_num++))
 done
 
-echo "PostgreSQL started at $POSTGRES_HOST_AND_PORT"
+echo "PostgreSQL started at ${POSTGRES_HOST}:${POSTGRES_PORT}"
 echo "Running seed script..."
 
 python ${SEED_SCRIPT_PATH}
