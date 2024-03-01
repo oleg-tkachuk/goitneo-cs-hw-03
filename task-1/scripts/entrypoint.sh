c8891dc9a8c9d4fbdf1dbb8ecf86c3422ccd293c
Oleg Tkachuk <oleg-tkachuk@users.noreply.github.com>
2024-03-01 22:22:30 +0200

    refactor(task-1): entrypoint.sh

--- task-1/scripts/entrypoint.sh
+++ task-1/scripts/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+SELF_NAME=$(basename "$0" .sh)
 POSTGRES_DB=${POSTGRES_DB:-"task_manager"}
 POSTGRES_USER=${POSTGRES_USER:-"db_admin"}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
@@ -12,21 +13,21 @@ attempt_num=1
 SEED_SCRIPT_PATH="/app/seed.py"
 SEED_SCRIPT_ARGS="--dbname ${POSTGRES_DB} --user ${POSTGRES_USER} --password ${POSTGRES_PASSWORD} --host ${POSTGRES_HOST} --port ${POSTGRES_PORT}"
 
-echo "Waiting for postgres to start..."
-echo "Connections details: ${POSTGRES_HOST}:${POSTGRES_PORT}"
+echo "${SELF_NAME}: Waiting for postgres to start..."
+echo "${SELF_NAME}: Connections details: ${POSTGRES_HOST}:${POSTGRES_PORT}"
 
 while ! nc -z ${POSTGRES_HOST} ${POSTGRES_PORT}; do
   sleep 0.1
   if [[ ${attempt_num} -eq ${MAX_ATTEMPTS} ]]; then
-    echo "Timeout waiting for PostgreSQL at ${POSTGRES_HOST}:${POSTGRES_PORT}"
+    echo "${SELF_NAME}: Timeout waiting for PostgreSQL at ${POSTGRES_HOST}:${POSTGRES_PORT}"
     exit 1
   fi
-  echo "PostgreSQL not available yet, retrying (${attempt_num}/${MAX_ATTEMPTS})..."
+  echo "${SELF_NAME}: PostgreSQL not available yet, retrying (${attempt_num}/${MAX_ATTEMPTS})..."
   ((attempt_num++))
 done
 
-echo "PostgreSQL started at ${POSTGRES_HOST}:${POSTGRES_PORT}"
-echo "Running seed script..."
+echo "${SELF_NAME}: PostgreSQL started at ${POSTGRES_HOST}:${POSTGRES_PORT}"
+echo "${SELF_NAME}: Running seed script..."
 
 python ${SEED_SCRIPT_PATH} ${SEED_SCRIPT_ARGS}
 exit $?
